b1c1ce416342593dbf712bb178cf79b48cbb632a
JoshuaLoveCoding <anqi6591@gwu.edu>
2018-11-04 11:47:29 -0500

    Seventh commit

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Joshua on 11/4/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Joshua on 11/4/18.
+//  Copyright © 2018 Joshua. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
